813db242a9ee28d0e6996d7d0f6b566f05968482
Nanna Amankwah <na6404am-s@student.lu.se>
2023-02-22 16:41:15 +0100

    "Kopplat gränssnitt med metoder från WS"

--- src/se/lu/ics/ControllerWS.java
+++ src/se/lu/ics/ControllerWS.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.rmi.RemoteException;
 
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 
 public class ControllerWS {
@@ -29,20 +30,48 @@ public class ControllerWS {
 		
 		private void declareListener(){
 			
+			JComboBox comboBoxViewAll = app.getComboBoxViewAll();
+
+			comboBoxViewAll.addItem("Product Category");
+			comboBoxViewAll.addItem("Customers");
+			comboBoxViewAll.addItem("Orders");
+			comboBoxViewAll.addItem("Products");
+			comboBoxViewAll.addItem("Stores");
+
+			
 			app.getBtnSelectAll().addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					
 					WebService1Soap service = new WebService1SoapProxy();
+					String selectedItem = (String)app.getComboBoxViewAll().getSelectedItem();
 					
 					try {
+						switch(selectedItem) {
+							
+						case "Product Category":
+							ProductCategory [] categories = service.getCategories();
+							
+							for(ProductCategory pc : categories) {
+								System.out.print(pc.getCategoryID());
+								app.getTextAreaViewAll().setText("The ID is: " + pc.getCategoryID());
+							}
+							break;
+							
+						case "Customers":
+								Customer [] customers = service.getCustomers();
+								
+								for(Customer c : customers) {
+									System.out.print(c.getName());
+									app.getTextAreaViewAll().setText("The ID is: " + c.getName());
+
+								}
+								break;
+							
 						
 						
-						
-						Customer [] customers = service.getCustomers();
-						
-						for(Customer c : customers) {
-							System.out.print(c.getName());
 						}
+						
+						
 						
 						
 					} catch (RemoteException e1) {
